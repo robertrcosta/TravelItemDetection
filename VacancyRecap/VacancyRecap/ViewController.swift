922e1e5de7341f7a32de76c889cae18dd0fc58f6
Gerard Riera <gerard.rierra@sunwebgroup.com>
2021-06-17 12:34:54 +0200

    Created base project

---
+++ VacancyRecap/VacancyRecap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VacancyRecap
+//
+//  Created by Gerard Riera Puig on 17/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
