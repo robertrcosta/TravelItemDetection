4e6dc90469d1378c2dff3fdfba323084faf9f3f7
Gerard Riera <gerard.rierra@sunwebgroup.com>
2021-06-18 11:39:47 +0200

    Created ResultVC

---
+++ TravelItemsDetection/Sources/Views/Result/ResultPresenter.swift
@@ -0,0 +1,17 @@
+//
+//  ResultPresenter.swift
+//  TravelItemsDetection
+//
+//  Created by Gerard Riera Puig on 18/6/21.
+//
+
+import Foundation
+
+class ResultPresenter {
+    var items = [
+        Item(name: "ToothBrush",
+             phrase: "How do you pretend to brush your teeth?"),
+        Item(name: "Passport",
+             phrase: "How do you pretend to travel without documentation?")
+    ]
+}
